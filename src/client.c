ea1e0d30dc5936fb7b3209801c995f9a192837c1
Yves Orton <yves.orton@booking.com>
2014-06-05 21:50:47 +0200

    fixup declaration after statement issues
    what can I say? I am old skool.

--- src/client.c
+++ src/client.c
@@ -14,14 +14,23 @@ static __inline__ unsigned long long rdtsc(void)
   return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
 
+#define MAGIC 0xDEADBEEF
+#define DATA_SIZE 128
 int main(int ac, char **av) {
+    int do_max = 0;
+    int data_size = DATA_SIZE;
+    int fd = 0;
+    unsigned long i = 0;
+    unsigned long ms;
+    clock_t start;
+    char DATA[DATA_SIZE];
+    unsigned int *magic_place;
+
     srand(time(NULL) + rdtsc());
     if (ac < 2)
         SAYX(EXIT_FAILURE,"%s remote-ip:remote-port",av[0]);
     socketize(av[1],&s);
     open_socket(&s,DO_NOTHING);
-    int do_max = 0;
-    int data_size = 128;
     if (ac > 2)
         do_max = atoi(av[2]);
 
@@ -31,7 +40,7 @@ int main(int ac, char **av) {
         SAYX(EXIT_FAILURE,"data_size: %d is too small",data_size);
 
     _D("processing max %d with size %d",do_max,data_size);
-    int fd = 0;
+
 #ifdef USE_SERVER
     if (bind(s.socket, (struct sockaddr *) &s.sa.in, s.addrlen) )
         SAYPX("bind %s",s.to_string);
@@ -46,23 +55,23 @@ int main(int ac, char **av) {
     }
 #endif
     fd = fd ? fd : s.socket;
-    clock_t start = clock();
-    unsigned long i = 0;
-    char DATA[data_size];
-    unsigned int *magic_place = (unsigned int *) &DATA[sizeof(DATA) - 10];
+    start = clock();
+    magic_place = (unsigned int *) &DATA[sizeof(DATA) - 10];
     DATA[sizeof(DATA) - 1] = '\n';
-    #define MAGIC 0xDEADBEEF
     *magic_place = MAGIC;
     while (do_max == 0 || i < do_max) {
+        int rc;
+        #ifdef USE_SERVER
+        int expected= 0;
+        #endif
+
         i++;
         #ifdef USE_SERVER
-            int rc;
             #ifdef RANDOM_DEATH
             if (s.type != SOCK_DGRAM && (random() % 100000000) == 42)
                 SAYX(EXIT_SUCCESS,"death decided by chance");
             #endif
             *magic_place = 0x11111111;
-            int expected;
             if (recv(fd,&expected,4,MSG_WAITALL) != 4)
                 SAYPX("recv");
             if (expected != sizeof(DATA))
@@ -75,12 +84,12 @@ int main(int ac, char **av) {
             if (*magic_place != MAGIC)
                 SAYX(EXIT_FAILURE,"magic not found in the received packet %x recv: %d, data_size: %d",*magic_place,rc,data_size);
         #else
-            int rc = sendto(fd,DATA,sizeof(DATA),0,(struct sockaddr*) &s.sa.in,s.addrlen);
+            rc = sendto(fd,DATA,sizeof(DATA),0,(struct sockaddr*) &s.sa.in,s.addrlen);
             if (rc < 0)
                 SAYPX("sendto");
         #endif
     }
-    unsigned long ms = (clock() - start) * 1000 / CLOCKS_PER_SEC;
+    ms = (clock() - start) * 1000 / CLOCKS_PER_SEC;
     _D(PREFIX " %lu with size %d for %lu ms (expected %f per second)",i,data_size,ms,i/((double)ms/1000));
     return(0);
 }
