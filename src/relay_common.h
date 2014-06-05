fd49979f0fca5bd694241aa952e9436eec01f2bf
Yves Orton <yves.orton@booking.com>
2014-06-05 20:49:47 +0200

    clean up

--- src/relay_common.h
+++ src/relay_common.h
@@ -10,9 +10,12 @@
 #define FORMAT(fmt,arg...) fmt " [%s():%s:%d @ %u]\n",##arg,__func__,__FILE__,__LINE__,(unsigned int) time(NULL)
 #define _E(fmt,arg...) fprintf(stderr,FORMAT(fmt,##arg))
 
-#define SAYX(rc,fmt,arg...) do {    \
-    _E(fmt,##arg);                  \
-    exit(rc);                       \
-} while(0)
+#define STMT_START do
+#define STMT_END while (0)
+
+#define SAYX(rc,fmt,arg...) STMT_START {    \
+    _E(fmt,##arg);                          \
+    exit(rc);                               \
+} STMT_END
 
 #endif
