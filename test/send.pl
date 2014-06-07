9fa6a33700c14412491c951c8be0d95d4ee3c2d3
Borislav Nikolov <borislav.nikolov@booking.com>
2014-06-07 15:06:51 +0200

    send.pl: add nanosleep option

--- test/send.pl
+++ test/send.pl
@@ -7,27 +7,27 @@ use Time::HiRes qw(time nanosleep);
 my $file = "";
 my $port = 10000;
 my $host = "localhost";
-GetOptions ("port=i" => \$port,
-            "file=s" => \$file,
-            "host=s" => \$host);
-die "usage: $0 --port=1000 --host=localhost --file=/tmp/example.txt" unless $file && $port && $host;
+my $ns   = 1000;
+GetOptions ("port=i"      => \$port,
+            "file=s"      => \$file,
+            "nanosleep=i" => \$ns,
+            "host=s"      => \$host);
+die "usage: $0 --port=10000 --host=localhost --file=/tmp/example.txt" unless $file && $port && $host;
 
 open(my $fh,'<',$file);
 my $data = do { local $/; <$fh> };
 close($fh);
 
 my $i = 0;
-my $t0 = time();
-my $every = 1_00_000;
+my $now = time();
 my $remote = IO::Socket::INET->new(Proto => 'udp',PeerAddr => $host,PeerPort => $port) or die $!;
 while (1) {
-    if ($i++ > $every) {
-        my $now = time();
-        my $s = ($now - $t0);
-        printf "SENDING $i packets took: %.2f s [ %.2f packets per second ]\n", $s,$i/$s;
-        $t0 = $now;
+    if (int($now) != int(time())) {
+        print "SENDING $i packets per second\n";
+        $now = time();
         $i = 0;
     }
-
     $remote->send($data);
+    $i++;
+    nanosleep($ns) if $ns;
 }
