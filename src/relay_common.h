4ac58075279a3c11f82fe63974462ec5f7ad8915
Damian Gryski <damian@gryski.com>
2014-06-08 22:06:40 +0200

    Remove t_fprintf macros
    The throttling code was removed in c7bcf846db8c6c3cd4f062082c2259c549ab955f

--- src/relay_common.h
+++ src/relay_common.h
@@ -40,8 +40,6 @@
 } STMT_END
 
 #define _D(fmt, arg...) printf(FORMAT(fmt, ##arg))
-#define _TD(fmt, arg...) t_fprintf(THROTTLE_DEBUG, stdout, FORMAT(fmt, ##arg))
-#define _TE(fmt, arg...) t_fprintf(THROTTLE_ERROR, stdout, FORMAT(fmt, ##arg))
 
 #define SAYPX(fmt, arg...) SAYX(EXIT_FAILURE, fmt " { %s }", ##arg, errno ? strerror(errno) : "undefined error");
 #define _ENO(fmt, arg...) _E(fmt " { %s }", ##arg, errno ? strerror(errno) : "undefined error");
