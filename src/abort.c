ba621e5af0dc1b819217609b7d920fd03001912b
Yves Orton <yves.orton@booking.com>
2014-06-08 10:59:05 +0200

    add abort.c

---
+++ src/abort.c
@@ -0,0 +1,33 @@
+#include "abort.h"
+#include "relay_threads.h"
+#define DIE 1
+#define RELOAD 2
+
+volatile uint32_t ABORT = 0;
+
+void set_abort_bits(uint32_t v) {
+    RELAY_ATOMIC_OR(ABORT, v);
+}
+
+void unset_abort_bits(uint32_t v) {
+    v= ~v;
+    RELAY_ATOMIC_AND(ABORT, v);
+}
+
+void set_aborted() {
+    set_abort_bits(DIE);
+}
+
+uint32_t get_abort_val() {
+    return RELAY_ATOMIC_READ(ABORT);
+}
+
+uint32_t not_aborted() {
+    uint32_t v= RELAY_ATOMIC_READ(ABORT);
+    return (v & DIE) == 0 ? 1 : 0;
+}
+
+uint32_t is_aborted() {
+    uint32_t v= RELAY_ATOMIC_READ(ABORT);
+    return (v & DIE) == 0 ? 0 : 1;
+}
---
+++ src/abort.h
@@ -0,0 +1,13 @@
+#include "relay_threads.h"
+#include "relay_common.h"
+#include <stdint.h>
+#define DIE 1
+#define RELOAD 2
+
+void set_abort_bits(uint32_t v);
+void unset_abort_bits(uint32_t v);
+void set_aborted();
+uint32_t get_abort_val();
+uint32_t not_aborted();
+uint32_t is_aborted();
+
