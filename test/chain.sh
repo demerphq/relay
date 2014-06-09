7c63af37d236b453bd93d3f26fd5575c9fbed309
Yves Orton <yves.orton@booking.com>
2014-06-09 11:17:42 +0200

    tweaks to chain.sh to make it easier to control how many relays we start
    this should take arguments but my bash is terrible

--- test/chain.sh
+++ test/chain.sh
@@ -1,18 +1,23 @@
 export RELAY=../bin/clang/relay
 killall -9 relay
-export THIS_PORT=10000
-export NEXT_PORT=10001
-export LAST_PORT=9003
+export FIRST_PORT=10000
+export FIRST_PORT_PLUS_ONE=10001
+export LAST_PORT=10009
+export LISTENER_PORT=9003
+
+####################################################
+export THIS_PORT=$FIRST_PORT
+export NEXT_PORT=$FIRST_PORT_PLUS_ONE
 export PROTO=upd
 
-for NEXT_PORT in {10001..10001}
+for NEXT_PORT in $(seq $FIRST_PORT_PLUS_ONE $LAST_PORT)
 do
     $RELAY $PROTO@localhost:$THIS_PORT tcp@localhost:$NEXT_PORT &
     THIS_PORT=$NEXT_PORT
     PROTO=tcp
 done
 
-$RELAY tcp@localhost:$THIS_PORT tcp@localhost:$LAST_PORT &
+$RELAY tcp@localhost:$THIS_PORT tcp@localhost:$LISTENER_PORT &
 ps auwx | grep relay
 ../test/simple-listener.pl
 killall relay
