02d576445eae6bc746c33529df387f709572f8ed
Yves Orton <yves.orton@booking.com>
2014-06-13 20:56:01 +0200

    rework log conf

--- src/log.h
+++ src/log.h
@@ -2,22 +2,24 @@
 #define _LOG_H
 
 #ifdef DEBUGGING
-#define _DEBUG_ARGS __func__, __FILE__, __LINE__,
-#define DEBUG_FMT  " %s():%s:%d "
+#define _DEBUG_ARGS ,pthread_self(), __func__, __FILE__, __LINE__
+#define DEBUG_FMT  " th:%lu %s():%s:%d"
 #else
 #define _DEBUG_ARGS
 #define DEBUG_FMT
 #endif
 
-#define FORMAT(fmt, arg...) "[$$: %u epoch: %u th:%lu" DEBUG_FMT "] " fmt "\n", \
-    getpid(), (unsigned int) time(NULL), pthread_self() _DEBUG_ARGS, ## arg
+#define FORMAT(type, fmt, arg...) "[$$: %u epoch: %u" DEBUG_FMT " %s ] " fmt "\n", \
+    getpid(), (unsigned int) time(NULL) _DEBUG_ARGS, (type), ## arg
 
-#define WARN(fmt, arg...) fprintf(stderr, FORMAT(fmt, ## arg))
-#define SAY(fmt, arg...) printf(FORMAT(fmt, ## arg))
+#define PERR(type, fmt, arg...) fprintf(stderr, FORMAT(type,fmt, ## arg))
 
-#define DIE_RC(rc, fmt, arg...) STMT_START {    \
-    WARN(fmt, ## arg);                          \
-    exit(rc);                                   \
+#define WARN(fmt, arg...) PERR("WARN",fmt, ## arg)
+#define SAY(fmt, arg...) PERR("INFO",fmt, ## arg)
+
+#define DIE_RC(rc, fmt, arg...) STMT_START {                 \
+    PERR(rc == EXIT_FAILURE ? "FATAL" : "QUIT",fmt, ## arg); \
+    exit(rc);                                                \
 } STMT_END
 
 
