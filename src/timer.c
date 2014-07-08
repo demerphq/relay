f18472ab8b9a2434008389659f9211fb7b2908b9
Yves Orton <yves.orton@booking.com>
2014-07-08 22:39:00 +0200

    timer.c: fix whitespace

--- src/timer.c
+++ src/timer.c
@@ -6,6 +6,6 @@ int get_time( mytime_t *t) {
 }
 
 uint64_t elapsed_usec(mytime_t *start_time, mytime_t *end_time) {
-    return ( ( end_time->tv_sec - start_time->tv_sec ) * 1000000 ) 
+    return ( ( end_time->tv_sec - start_time->tv_sec ) * 1000000 )
            + end_time->tv_usec - start_time->tv_usec;
 }
