41aaa9956f16b70187790b7827b59579c279a241
Yves Orton <yves.orton@booking.com>
2014-07-08 22:57:11 +0200

    remove log.c and switch to using syslog

--- src/log.h
+++ src/log.h
@@ -1,6 +1,16 @@
 #ifndef _LOG_H
 #define _LOG_H
 
+#include <syslog.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+
+#define OUR_FACILITY LOG_LOCAL5
+
 #ifdef DEBUGGING
 #define _DEBUG_ARGS ,pthread_self(), __func__, __FILE__, __LINE__
 #define DEBUG_FMT  " th:%lu %s():%s:%d"
@@ -9,24 +19,45 @@
 #define DEBUG_FMT
 #endif
 
-#define FORMAT(type, fmt, arg...) "[$$: %u epoch: %u" DEBUG_FMT " %s ] " fmt "\n", \
-    getpid(), (unsigned int) time(NULL) _DEBUG_ARGS, (type), ## arg
+#define TS_LEN 30
 
-#include "config.h"
-extern struct config CONFIG;
-#define PERR(type, fmt, arg...) fprintf( CONFIG.logfh, FORMAT(type,fmt, ## arg))
+struct _ts {
+    time_t t;
+    struct tm tm;
+    char str[TS_LEN];
+};
 
-#define WARN(fmt, arg...) PERR("WARN",fmt, ## arg)
-#define SAY(fmt, arg...) PERR("INFO",fmt, ## arg)
+#define _LOG(type, fmt, arg...) STMT_START {                                        \
+    struct _ts *_ts;\
+                                                                                    \
+    _ts= malloc(sizeof(struct _ts));                                                \
+    (void)time(&_ts->t);                                                            \
+    (void)localtime_r(&_ts->t, &_ts->tm);                                           \
+                                                                                    \
+    strftime(_ts->str, TS_LEN, "%Y-%m-%d %H:%M:%S", &_ts->tm);                      \
+    syslog(                                                                         \
+            (OUR_FACILITY | (LOG_ ## type)),                                        \
+            "[%4.4s %s" DEBUG_FMT "] " fmt "\n", "" #type, _ts->str _DEBUG_ARGS, ## arg \
+    );                                                                              \
+    free(_ts);                                                                      \
+} STMT_END
 
-#define DIE_RC(rc, fmt, arg...) STMT_START {                 \
-    PERR(rc == EXIT_FAILURE ? "FATAL" : "QUIT",fmt, ## arg); \
-    exit(rc);                                                \
+#define WARN(fmt, arg...) _LOG(WARNING, fmt, ## arg)
+#define SAY(fmt, arg...) _LOG(INFO, fmt, ## arg)
+
+#define DIE_RC(rc, fmt, arg...) STMT_START {                    \
+    if (rc == EXIT_FAILURE) {                                   \
+        _LOG(CRIT, fmt, ## arg);                                \
+    } else {                                                    \
+        _LOG(NOTICE, fmt, ## arg);                              \
+    }                                                           \
+    exit(rc);                                                   \
 } STMT_END
 
 
 #define DIE(fmt, arg...) \
     DIE_RC(EXIT_FAILURE, fmt " { %s }", ##arg, errno ? strerror(errno) : "undefined error");
+
 #define WARN_ERRNO(fmt, arg...) \
     WARN(fmt " { %s }", ## arg, errno ? strerror(errno) : "undefined error");
 
