2a015cec732068fbf0bf770e7d85b5abf710ff75
Jarkko Hietaniemi <jarkko.hietaniemi@booking.com>
2014-10-07 15:42:14 +0200

    Add display of MB/s.
    Use HiRes time() only for the total rate, makes no sense
    to use it in the loop and keep int():ing it, and output
    the epoch timestamp.
    
    Default the file to out.srl.

--- test/send.pl
+++ test/send.pl
@@ -1,39 +1,63 @@
 #!/usr/bin/perl
+
 use strict;
 use IO::Socket;
 use Data::Dumper;
 use Getopt::Long;
-use Time::HiRes qw(time nanosleep);
-my $file = "";
+use Time::HiRes;
+my $file = "out.srl";
 my $port = 10000;
 my $host = "localhost";
 my $ns   = 1000;
 my $count;
-my $usage= !GetOptions ("port=i"      => \$port,
-            "file=s"      => \$file,
-            "nanosleep=i" => \$ns,
-            "count=i"     => \$count,
-            "host=s"      => \$host);
-die "usage: $0 --port=10000 --host=localhost --file=/tmp/example.txt" if $usage or !($file && $port && $host);
+
+die "usage: $0 --port=10000f --host=localhost --file=/tmp/example.txt"
+    unless (GetOptions("port=i"      => \$port,
+		       "file=s"      => \$file,
+		       "nanosleep=i" => \$ns,
+		       "count=i"     => \$count,
+		       "host=s"      => \$host) &&
+	    $count);
 
 open(my $fh,'<',$file);
 my $data = do { local $/; <$fh> };
 close($fh);
+my $MB = length($data) / 1024**2;
 
-my $i = 0;
-my $last= 0;
-my $start= my $now = time();
-my $remote = IO::Socket::INET->new(Proto => 'udp',PeerAddr => $host,PeerPort => $port) or die $!;
+my $packets = 0;
+my $last_packets = 0;
+my $start_hires = Time::HiRes::time();
+my $now_hires   = $start_hires;
+my $last_time;
+my $remote = IO::Socket::INET->new(Proto => 'udp',
+				   PeerAddr => $host,
+				   PeerPort => $port) or die "$0: $!";
 while (1) {
-    if (int($now) != int(time())) {
-        print "SENDING ", $i - $last, " packets per second\n";
-        $now = time();
-        $last= $i;
+    my $now = time();
+    if ($last_time != $now) {
+	if ($last_time) {
+	    my $sent_packets = $packets - $last_packets;
+	    if ($now > $last_time) {
+		printf("%d SENDING %.2f packets/sec (%.2f MB/s)\n",
+		       $now, $sent_packets,
+		       $MB * $sent_packets / ($now - $last_time));
+	    }
+	}
+	$last_time = $now;
+        $last_packets = $packets;
     }
     $remote->send($data);
-    $i++;
-    last if $count and $i >= $count;
-    nanosleep($ns) if $ns;
+    $packets++;
+    last if $count and $packets >= $count;
+    Time::HiRes::nanosleep($ns) if $ns;
 }
-$now = time();
-printf "Sent %d packets at %.2f packets per second\n", $i, $i / ($now - $start);
+$now_hires = Time::HiRes::time();
+my $took = $now_hires - $start_hires;
+if ($took > 0) {
+    printf("%d SENT %d packets (%.2f MB) at %.2f packets/second (%.2f MB/sec)\n",
+	   time(), $packets, $MB * $packets, $packets / $took, $MB * $packets / $took);
+} else {
+    die "$0: took less time than nothing\n";
+}
+
+exit(0);
