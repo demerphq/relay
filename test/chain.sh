a66c2a307365f39725e9672a3d93cb247cdc6c57
Jarkko Hietaniemi <jarkko.hietaniemi@booking.com>
2014-10-08 16:26:03 +0200

    Add comments, make more flexible (allow different chain length).

--- test/chain.sh
+++ test/chain.sh
@@ -1,31 +1,43 @@
+# Create a chain of relays, $RELAY_COUNT of them.
+#
+# The first relay listens at UDP port $FIRST_PORT
+# and relays to TCP port plus one. The rest of
+# the relays are all TCP relays, with ports plus one.
+#
+# The last relay is connected to a test listener.
+
+export FIRST_PORT=10000
+export FIRST_PORT_PLUS_ONE=$(expr $FIRST_PORT + 1)
+export RELAY_COUNT=10
+export LAST_PORT=$(expr $FIRST_PORT + $RELAY_COUNT - 1)
+export LISTENER_PORT=9003
+
 export RELAY=../bin/relay
 #export RELAY=../bin/relay.clang
 killall -9 relay
-export FIRST_PORT=10000
-export FIRST_PORT_PLUS_ONE=10001
-export LAST_PORT=10009
-export LISTENER_PORT=9003
-
-####################################################
-export THIS_PORT=$FIRST_PORT
-export NEXT_PORT=$FIRST_PORT_PLUS_ONE
-export PROTO=udp
 
 if test ! -f $RELAY; then
     echo "$0: No relay $RELAY, aborting."
     exit 1
 fi
 
+####################################################
+
+export THIS_PORT=$FIRST_PORT
+export PROTO=udp
+
 for NEXT_PORT in $(seq $FIRST_PORT_PLUS_ONE $LAST_PORT)
 do
-    echo $RELAY $PROTO@localhost:$THIS_PORT tcp@localhost:$NEXT_PORT 
-    $RELAY $PROTO@localhost:$THIS_PORT tcp@localhost:$NEXT_PORT &
+    CMD="$RELAY $PROTO@localhost:$THIS_PORT tcp@localhost:$NEXT_PORT"
+    echo "$CMD"
+    $CMD &
     THIS_PORT=$NEXT_PORT
     PROTO=tcp
 done
 
-echo $RELAY tcp@localhost:$THIS_PORT tcp@localhost:$LISTENER_PORT
-$RELAY tcp@localhost:$THIS_PORT tcp@localhost:$LISTENER_PORT &
+CMD="$RELAY tcp@localhost:$THIS_PORT tcp@localhost:$LISTENER_PORT"
+echo "$CMD"
+$CMD &
 ps auwx | grep relay
 ../test/simple-listener.pl
 killall relay
