429d5c826894e2f76e5efd838f63519b8266f2b7
Jarkko Hietaniemi <jarkko.hietaniemi@booking.com>
2014-10-17 19:34:17 +0200

    If we receive from TCP, for the payload bytes skip the length.
    Otherwise for tcp->tcp relays we will have the length twice (on output)
    when we should have it once, and for tcp->udp relays we have the
    length once, when we should not have it.
    
    (udp->udp and udp->tcp relays are fine since the length is not there.)

--- src/relay.c
+++ src/relay.c
@@ -195,7 +195,7 @@ static int tcp_read(tcp_server_context_t * ctxt, nfds_t i)
 	}
 
 	if (client->pos >= EXPECTED_PACKET_SIZE(client) + EXPECTED_HEADER_SIZE) {
-	    buf_to_blob_enqueue(client->buf, EXPECTED_PACKET_SIZE(client));
+	    buf_to_blob_enqueue(client->buf + sizeof(uint32_t), EXPECTED_PACKET_SIZE(client));
 
 	    client->pos -= EXPECTED_PACKET_SIZE(client) + EXPECTED_HEADER_SIZE;
 	    if (client->pos > 0) {
