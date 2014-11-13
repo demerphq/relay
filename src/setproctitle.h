438ba9c9e4506f8a6b1218a31ca614d9bc4ddea9
Jarkko Hietaniemi <jarkko.hietaniemi@booking.com>
2014-11-13 21:55:01 +0100

    No need for extern here.

--- src/setproctitle.h
+++ src/setproctitle.h
@@ -1,9 +1,9 @@
 #ifndef UTIL_LINUX_SETPROCTITLE_H
 #define UTIL_LINUX_SETPROCTITLE_H
 
-extern void initproctitle(int argc, char **argv);
-extern void setproctitle(const char *txt);
-extern void destroy_proctitle(void);
+void initproctitle(int argc, char **argv);
+void setproctitle(const char *txt);
+void destroy_proctitle(void);
 
 #define STR_AND_LEN(s,l) (( l= sizeof(s "")), (s ""))
 
