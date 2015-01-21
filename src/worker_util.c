3b1ce5e1cf2952d13b3b3b650e5607cfdebd81c1
Jarkko Hietaniemi <jarkko.hietaniemi@booking.com>
2015-01-21 09:46:01 +0100

    Don't keep doubling the wait period, have a limit.

--- src/worker_util.c
+++ src/worker_util.c
@@ -6,15 +6,21 @@ relay_socket_t *open_output_socket_eventually(struct worker_base *base)
     const config_t *config = base->config;
     relay_socket_t *sck = NULL;
     int nap = config->sleep_after_disaster_millisec;
+    int max = config->max_socket_open_wait_millisec;
 
     while (!RELAY_ATOMIC_READ(base->stopping) && !sck) {
 	if (open_socket(&base->output_socket, DO_CONNECT, config->server_socket_sndbuf_bytes, 0)) {
 	    sck = &base->output_socket;
 	} else {
-	    /* no socket - wait a while, double the wait, and then redo the loop */
+	    /* no socket - wait a while, double the wait (up to a limit), and then redo the loop */
 	    SAY("waiting %d millisec to retry socket %s", nap, base->output_socket.to_string);
 	    worker_wait_millisec(nap);
-	    nap = 2 * nap + (time(NULL) & 31);	/* "Random" fuzz of up to 0.031s. */
+	    if (nap < max) {
+		nap = 2 * nap + (time(NULL) & 31);	/* "Random" fuzz of up to 0.031s. */
+	    }
+	    if (nap > max) {
+		nap = max;
+	    }
 	}
     }
 
